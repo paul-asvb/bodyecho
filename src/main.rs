9ddccb9c375e4e83e3519bb76fe5382c55e5cb9e
Paul Mayer <paul@paul-mayer.eu>
2025-10-26 14:17:18 +0100

    feat: init screen

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ fn main() {
     init_panic_hook();
 
     App::new()
+        .insert_resource(SplashTimer(Timer::from_seconds(3.0, TimerMode::Once)))
         .add_plugins(DefaultPlugins.set(WindowPlugin {
             primary_window: Some(Window {
                 title: "Bodyecho".into(),
@@ -16,6 +17,7 @@ fn main() {
             ..default()
         }))
         .add_systems(Startup, setup)
+        .add_systems(Update, splash_screen_timer)
         .run();
 }
 
@@ -31,9 +33,52 @@ fn setup(mut commands: Commands) {
         transform: Transform::from_xyz(0.0, 0.0, 0.0),
         ..default()
     });
+
+    commands
+        .spawn(NodeBundle {
+            style: Style {
+                width: Val::Percent(100.0),
+                height: Val::Percent(100.0),
+                align_items: AlignItems::Center,
+                justify_content: JustifyContent::Center,
+                ..default()
+            },
+            background_color: BackgroundColor(Color::srgba(0.0, 0.0, 0.0, 0.35)),
+            ..default()
+        })
+        .insert(SplashScreen)
+        .with_children(|parent| {
+            parent.spawn(TextBundle::from_section(
+                "Bodyecho",
+                TextStyle {
+                    font_size: 72.0,
+                    color: Color::WHITE,
+                    ..default()
+                },
+            ));
+        });
 }
 
 #[cfg(target_arch = "wasm32")]
 fn init_panic_hook() {
     console_error_panic_hook::set_once();
+}
+
+#[derive(Resource)]
+struct SplashTimer(Timer);
+
+#[derive(Component)]
+struct SplashScreen;
+
+fn splash_screen_timer(
+    time: Res<Time>,
+    mut timer: ResMut<SplashTimer>,
+    mut commands: Commands,
+    splash_query: Query<Entity, With<SplashScreen>>,
+) {
+    if timer.0.tick(time.delta()).finished() {
+        for entity in splash_query.iter() {
+            commands.entity(entity).despawn_recursive();
+        }
+    }
 }
